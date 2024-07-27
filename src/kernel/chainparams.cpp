d73885c1daf7d946cb06433bc948bfec18ce1e6b
Tal Ben Arie <31064168+talbenarie@users.noreply.github.com>
2024-07-27 22:36:01 +0300

    Updated Genesis Block

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -121,10 +121,15 @@ public:
         m_assumed_blockchain_size = 600;
         m_assumed_chain_state_size = 10;
 
-        genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
+        // genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
+        // consensus.hashGenesisBlock = genesis.GetHash();
+        // assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
+        // assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+
+        genesis = CreateGenesisBlock(1722107319, 73864, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x0000d4e8c72910d7fccf3fde3820549a19b52fa5ac68feb24f5bf81df5c1c224"));
+        assert(genesis.hashMerkleRoot == uint256S("0xfdd1e52925d83973a309204cd93834c23dbd17ef6aafd485db77d48de9f29274"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
